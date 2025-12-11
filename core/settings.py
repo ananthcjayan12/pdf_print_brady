9568234de065cfd9935b71665ff229e8776005d5
ananthuuu <ananthcjayan12@gmail.com>
2025-12-11 09:40:25 +0530

    addded new ip again

--- core/settings.py
+++ core/settings.py
@@ -146,4 +146,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # CSRF settings
 CSRF_COOKIE_SECURE = False  # Set to True in production with HTTPS
 CSRF_COOKIE_HTTPONLY = False  # Allow JavaScript to access the CSRF cookie
-CSRF_TRUSTED_ORIGINS = ['http://vogo4s8kc0k84gcosg8g4k0g.82.112.235.216.sslip.io']
+CSRF_TRUSTED_ORIGINS = ['http://vogo4s8kc0k84gcosg8g4k0g.82.112.235.216.sslip.io','http://72.61.174.204:8302']
